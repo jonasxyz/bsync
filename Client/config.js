935bd64efcab31579f422fd56500a8776dff76d6
jonasxyz <54746338+jonasxyz@users.noreply.github.com>
2022-02-04 17:10:12 +0100

    Create config.js

---
+++ Client/config.js
@@ -0,0 +1,17 @@
+const config = module.export = {
+
+	client_name : "puppeteer", 						// must be unique
+	headless : false,
+
+	crawl_script : "puppeteer_synced.py", 
+	script_path : "/home/user/Schreibtisch/bsync/Client/puppeteer",
+
+	master_addr : "http://192.168.178.73:3000", 				// e.g. "http://localhost:3000"
+
+	disable_proxy : true, 							// set true to disable mitmproxy
+	har_destination : "/home/user/Schreibtisch/client/http_v2/",      	// e.g. "/home/user/http/"
+	proxy_host : "127.0.0.1",
+	proxy_port : "3031",
+	proxy_script_location : "/home/user/Schreibtisch/http/",
+
+};

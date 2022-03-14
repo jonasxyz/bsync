249c6c37b730f75fadb3f06bd1ce878f51d0bba4
jonasxyz <jbauchmu@gmx.de>
2022-03-14 13:07:14 +0100

    urldone changes

--- Server/config.js
+++ Server/config.js
@@ -1,18 +1,18 @@
 const config = module.exports = {
 
-    num_clients : 4,                  // set number of clients you want to connect
+    num_clients : 1,                  // set number of clients you want to connect
     ip : "http://192.168.178.73",     // insert master-server ip
     port : 3000,                      // set master-server port (default 3000)
     allowed_ping : 100,               // set allowed ping between clients and server
     url_list : "tranco100.csv",       // insert name of the list containing the websites to crawl (.txt or .csv)
-    calibration_runs : 10,             // set number of runs measuring the http access time between browsers (default 10)
+    calibration_runs : 1,             // set number of runs measuring the http access time between browsers (default 10)
     re_calibration : 100,             // repeat calibration afer number of websites crawled (default 100)
     
     timeout_ms : 60000,              // set milliseconds after which the browser is restarted when the urldone signal is not received
                                       // adjust that to the time browsers stay on the website e.g 4x
     website_attempts : 2,              // set number of attempts to crawl a website if errors occur before URL is skipped
 
-    test_run : true,                 // testing the synchronisation
+    test_run : false,                 // testing the synchronisation
     test_iterations : 1000,           // set number of test runs
 
     
